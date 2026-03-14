7b0dc65c920e3c13c7898300f662a8a2cd98a6e1
Walid Krati <wkrati@e2r6p3.1337.ma>
2026-03-14 04:29:26 +0000

    Starting output file

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,8 +1,10 @@
+from logging import config
 import sys
 from typing import Any
 
 from visualisation.parsing import parse_config
 from mazegen.generator import MazeGenerator
+from mazegen.show_the_exit import find_the_way
 from visualisation.display import run_display
 
 
@@ -18,6 +20,35 @@ def build_generator(config: dict[str, Any]) -> MazeGenerator:
     )
 
 
+def write_output_file(filename: str, maze: MazeGenerator,
+                      path: list[tuple[int, int]]) -> None:
+    """Write the maze and path in hexadecimal to a text file.
+    Args:
+            maze: Maze to save
+            filename: Output filename
+
+        File format:
+            - One hex digit per cell (walls encoding)
+            - One row per line
+            - Empty line
+            - Entry coordinates
+            - Exit coordinates
+            - Solution path (N, E, S, W moves)
+    """
+    with open(filename, 'w') as f:
+        # Write maze in hexadecimal
+        for row in maze:
+            f.write(''.join(format(cell, '') for cell in row) + '\n')
+        f.write('\n')
+        # Write entry coordinates
+        f.write(f"{maze.entry}\n")
+        # Write exit coordinates
+        f.write(f"{maze.exit}\n")
+        # Write solution path
+        f.write("".join(move for move in path) + "\n")
+        
+
+
 def main() -> None:
     """Program entry point."""
     if len(sys.argv) != 2:
@@ -31,10 +62,12 @@ def main() -> None:
         generator = build_generator(config)
 
         if config["PERFECT"]:
-            generator.generate_perfect_maze()
+            maze = generator.generate_perfect_maze()
         else:
-            generator.generate_imperfect_maze()
+            maze = generator.generate_imperfect_maze()
 
+        path = find_the_way(maze, config["ENTRY"], config["EXIT"])
+        write_output_file(config["OUTPUT_FILE"], generator, path)
         run_display(config, generator)
 
     except Exception as e:
